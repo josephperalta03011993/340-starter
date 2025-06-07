38d010c334557feb7ffa502fd00218400fc0221f
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-06-08 00:31:33 +0800

    update register new account working

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const baseController = require("./controllers/baseController")
 const utilities = require("./utilities/")
 const session = require("express-session")
 const pool = require('./database/')
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
@@ -37,7 +38,8 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 /* ***********************
  * View Engine and Templates
  *************************/
