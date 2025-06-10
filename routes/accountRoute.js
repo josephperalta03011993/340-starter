47fe4aaf58ab50f8f1c328b2c2fd35164016a74b
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-06-11 00:24:18 +0800

    week4 learning act and team work done

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,10 +3,29 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities");
+const regValidate = require('../utilities/account-validation')
 
 // Deliver login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/registration", utilities.handleErrors(accountController.buildRegister));
-router.post('/registration', utilities.handleErrors(accountController.registerAccount));
+
+// Process registration data
+router.post(
+    "/registration", 
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+);
+
+// Process the login attempt
+router.post(
+    "/login",
+    regValidate.loginRules(),
+    regValidate.checkLoginData,
+    utilities.handleErrors(accountController.loginAccount)
+    // (req, res) => {
+    //   res.status(200).send('login process')
+    // }
+);
 
 module.exports = router;
